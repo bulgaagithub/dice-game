3428eaedb73fdc22582db544c570404a6c423139
Erdenebulgan <erdenebulgandeveloper@gmail.com>
2020-02-02 05:35:13 +0800

    Toglogchiin eelhiig soligddog bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогч 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 // Тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 // Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
@@ -26,11 +26,42 @@ document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
+// Шоог шидэх эвент листенер
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1 - 6 доторх санамсаргүй нэг тоо гаргаж авна.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Шооны зургийг вэб дээр гаргаж ирнэ.
   diceDom.style.display = "block";
+
+  // Буусан санамсаргүй тоонд харгалзах зургийг вэб дээр гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
   // alert("Шоог шидлээ: " + diceNumber);
+
+  // Буусан тоо нь 1 ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ. Тоглогчийн ээлжийн оноог өөрчилнө.
+  var currentScore = document.getElementById("current-" + activePlayer);
+  if (diceNumber !== 1) {
+    // 1 -ээс ялгаатай тоо буулаа.
+    roundScore += diceNumber;
+    currentScore.textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+    roundScore = 0;
+    currentScore.textContent = 0;
+
+    // Хэрвээ идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг нэг болго.
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    // Улаан цэгийг шилжүүлэх кодыг хийнэ.
+    // toggle функц нь энэ тагийн класс дотор тухайн класс байвал устгана байхгүй бол нэмнэ.
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // Шоог түр алга болгоно.
+    diceDom.style.display = "none";
+  }
 });
 
 // global object ni function дотор гадна бүгдэд харагдана.
