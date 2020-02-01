969a054687b11b809a1ea37cba542c95d61d2ba8
Erdenebulgan <erdenebulgandeveloper@gmail.com>
2020-02-01 13:35:25 +0800

    Shoog shiddeg bolgoson

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шоо аль талаараа буусан бэ гэдгийг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // Dom oos id gaar ni elementiig olood ternii utgii update hiine.
 // Dom iin neg object butsaj irne.
@@ -20,11 +19,18 @@ var dice = Math.floor(Math.random() * 6) + 1;
 // innerHTML
 
 // Програм эхлэх бэлтгэл
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = 0; // илүү хурдтай ажилладаг. id хайж байгаа тохиолдолд энэ функцийг ашиглана.
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+  // alert("Шоог шидлээ: " + diceNumber);
+});
 
 // global object ni function дотор гадна бүгдэд харагдана.
-console.log("Шоо: ", dice);
